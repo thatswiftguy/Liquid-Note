7420ef92fb442b19984275a4fb76b06cffe869d3
Yasir <mdcode2021@gmail.com>
2025-09-13 00:19:59 +0530

    Initial Commit

---
+++ LiquidNote/LiquidNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidNoteApp.swift
+//  LiquidNote
+//
+//  Created by Yasir on 12/09/25.
+//
+
+import SwiftUI
+
+@main
+struct LiquidNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
