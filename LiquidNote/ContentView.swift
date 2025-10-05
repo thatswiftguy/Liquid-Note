50a420170e0bc9f968c56d5b8cfdfc3757016f91
Yasir <mdcode2021@gmail.com>
2025-10-05 19:51:56 +0530

    clean up

--- LiquidNote/ContentView.swift
+++ LiquidNote/ContentView.swift
@@ -7,16 +7,6 @@
 
 import SwiftUI
 
-/*
- Screens
- 1. Folders List
- 2. Create Folders
- 3. Notes List
- 4. Create/Edit Note
- 5. Search
- */
-
-
 struct ContentView: View {
     var body: some View {
         FolderList()
